5db989409a40f6d37b652aa58a3dbd01b72ae24c
narendar2706 <62331095+narendar2706@users.noreply.github.com>
2022-11-03 01:09:16 +0530

    Create Q_5.cs

---
+++ Q_5.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace problem5
+{
+    public class Q5{
+        public static double GetGrossPay(int hours){
+            double pay;
+            if(hours<57){
+                pay=hours*80;
+            }
+            else{
+                pay=((hours-56))*120+(56*80);
+            }
+            return pay;
+        }
+        public static double CalculateTax(double gpay){
+            double taxAmount=gpay*0.02;
+            return taxAmount;
+        }
+        public static double CalculateNetPay(int hours){
+            double g_pay=GetGrossPay(hours);
+            double net_pay=g_pay-CalculateTax(g_pay);
+            return net_pay;
+        }
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("enter the number of hours worked :");
+            int hours=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The calculated Net Pay is "+CalculateNetPay(hours));
+        }
+    }
+}
